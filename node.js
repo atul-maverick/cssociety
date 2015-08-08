f2cb85c35cee86f5a31bf8349db3fc26e5ac4788
Ryan Rogers <ryan.rogers@mavs.uta.edu>
2015-08-08 21:43:41 +0000

    Node server code uploaded

---
+++ node.js
@@ -0,0 +1,74 @@
+// Navigating to the workspace directory
+console.log('Starting directory: ' + process.cwd());
+try {
+  process.chdir('../../home/ubuntu/workspace');
+  console.log('New directory: ' + process.cwd());
+}
+catch (err) {
+  console.log('chdir: ' + err);
+}
+
+// Stolen from: Node.js for Front-End Developers by Garann Means (p. 9-10) 
+var port = 8080;
+var serverUrl = "172.17.28.185";
+
+var http = require("http");
+var path = require("path"); 
+var fs = require("fs"); 		
+
+console.log("Starting web server at " + serverUrl + ":" + port);
+
+http.createServer( function(req, res) {
+
+	var now = new Date();
+
+	var filename = req.url || "index.html";
+	var ext = path.extname(filename);
+	var localPath = __dirname;
+	var validExtensions = {
+		".html" : "text/html",			
+		".js": "application/javascript", 
+		".css": "text/css",
+		".txt": "text/plain",
+		".jpg": "image/jpeg",
+		".gif": "image/gif",
+		".png": "image/png",
+		".woff": "font/wof",
+		".woff2": "application/font-woff2"
+	};
+	var isValidExt = validExtensions[ext];
+
+	if (isValidExt) {
+		
+		localPath += filename;
+		path.exists(localPath, function(exists) {
+			if(exists) {
+				console.log("Serving file: " + localPath);
+				getFile(localPath, res, ext);
+			} else {
+				console.log("File not found: " + localPath);
+				res.writeHead(404);
+				res.end();
+			}
+		});
+
+	} else {
+		console.log("Invalid file extension detected: " + ext)
+	}
+
+}).listen(port, serverUrl);
+
+function getFile(localPath, res, mimeType) {
+	fs.readFile(localPath, function(err, contents) {
+		if(!err) {
+			res.setHeader("Content-Length", contents.length);
+			res.setHeader("Content-Type", mimeType);
+			res.statusCode = 200;
+			res.end(contents);
+		} else {
+			res.writeHead(500);
+			res.end();
+		}
+	});
+}
+
